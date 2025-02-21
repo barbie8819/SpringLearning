71ef9a201dc0d6e1b006611f5da8e8889c21f979
Anshi Patel <anshipatel881@gmail.com>
2025-02-21 16:29:31 +0530

    [Anshi] Add . Added Hello using query parameter

--- SpringLearning/src/main/java/com/Spring/SpringLearning/HelloController.java
+++ SpringLearning/src/main/java/com/Spring/SpringLearning/HelloController.java
@@ -1,9 +1,7 @@
 package com.Spring.SpringLearning;
 
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hello")
@@ -13,6 +11,11 @@ public class HelloController {
         public String sayHello() {
             return "Hello from BridgeLabz";
         }
+    @GetMapping("/query")
+    public String sayHelloWithName(@RequestParam String name) {
+        return "Hello " + name + " from BridgeLabz";
     }
+
+}
 
 
